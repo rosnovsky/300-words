4fc0b89b5e2391f73ed3d418dd4ce8e97facc8c5
Art Rosnovsky <artem@rosnovsky.us>
2023-04-23 05:02:24 +0000

    chore: middleware experiment

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+  url: 'https://usw2-promoted-terrapin-30708.upstash.io',
+  token: process.env.UPSTASH_TOKEN || ""
+})
+
+const ratelimit = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.slidingWindow(2, "10 s"),
+});
+
+export default async function middleware(
+  request: NextRequest,
+  event: NextFetchEvent,
+): Promise<Response | undefined> {
+  const ip = request.ip ?? "127.0.0.1";
+  const { success, pending, limit, reset, remaining } = await ratelimit.limit(ip);
+  return success ? NextResponse.next() : NextResponse.redirect(new URL("/blocked", request.url));;
+}
+
+export const config = {
+  matcher: "/",
+};
---
+++ src/pages/blocked.tsx
@@ -0,0 +1,10 @@
+export default function Blocked() {
+  return (
+    <div>
+      <main>
+        <h1>Access blocked.</h1>
+        <h3>Error 429</h3>
+      </main>
+    </div>
+  )
+}
