c03c0231975679838955bff8ed916bc057bc9d5e
Art Rosnovsky <art.rosnovsky@okta.com>
2023-04-24 16:26:43 -0700

    fix: fix missing edge config in `update`

--- src/pages/api/notes/update.ts
+++ src/pages/api/notes/update.ts
@@ -1,27 +1,30 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextRequest, NextResponse } from 'next/server';
 import type { Note } from '@/types'
 import { psClient } from '@/utils/planetscale';
 import { noteDataSchema, validateInput } from '@/utils/validatiors';
 import { errorHandler } from '@/utils/errorHandler';
 
+export const config = {
+  runtime: 'edge',
+};
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+  req: NextRequest,
+  res: NextResponse
 ) {
   if (req.method !== 'PUT') {
-    res.status(405).json({ error: 'Method not allowed' });
+    return new Response('Method not allowed', { status: 405 });
   } else {
     try {
-      const noteData: Note = req.body;
+      const noteData: Partial<Note> = await new Response(req.body).json()
 
       const value = validateInput(noteDataSchema, noteData)
 
-      if (!value) return res.status(400).json(value)
+      if (!value) return new Response('Invalid input', { status: 400 })
 
       const updatedNote = await psClient.execute('UPDATE notes SET content = ?, updated_at = CURRENT_TIMESTAMP() WHERE id = ?', [value.content, value.id]);
 
-      res.status(201).json({ success: true, message: updatedNote.rowsAffected });
+      return new Response('success', { status: 200 });
     } catch (error: any) {
       errorHandler(res, error, "Error updateing note");
     }
